8031539bfce98a5f4b357db516956a41110722e6
novandroromanda <novandroromanda@gmail.com>
2025-09-24 01:46:48 +0700

    First Commit

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+feather.replace();
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    if (navLinks.classList.contains("active")) {
+        navToggle.innerHTML = feather.icons["x"].toSvg();
+    } else {
+        navToggle.innerHTML = feather.icons["menu"].toSvg();
+    }
+});
+
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;  
+    const elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
