42e67cf47a3cf630af0b48694f2c6096d268350e
novandroromanda <novandroromanda@gmail.com>
2025-10-01 23:08:07 +0700

    feat: project desain

--- JS/script.js
+++ JS/script.js
@@ -17,7 +17,7 @@ function revealOnScroll() {
   for (let i = 0; i < reveals.length; i++) {
     const windowHeight = window.innerHeight;
     const elementTop = reveals[i].getBoundingClientRect().top;  
-    const elementVisible = 100;
+    const elementVisible = 80;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
